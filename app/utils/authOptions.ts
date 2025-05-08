5987385fd90f8c06cb12845b06dbbf43c05ead6e
Beichen-Ma <mabeichen12@gmail.com>
2025-05-08 13:55:44 -0400

    fix auth bug

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -8,6 +8,7 @@ export const authOptions: AuthOptions = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: "Credentials",
